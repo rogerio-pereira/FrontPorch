ea7c297f1158d69b015e419eb20248c111f2bbff
rogerio-pereira <13219168+rogerio-pereira@users.noreply.github.com>
2026-07-24 15:45:31 +0000

    style(api/ui): Fix code style (pint and lint)

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 
