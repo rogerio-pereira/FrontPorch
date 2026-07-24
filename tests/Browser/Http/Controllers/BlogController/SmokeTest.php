e9a7db4d5773206abc70ae94feda62e25586fcaf
Rogerio Pereira <hi@rogeriopereira.dev>
2026-07-24 13:03:13 -0400

    fix(ui): remove unused Welcome layout exception
    Home and marketing pages no longer need a special-case null layout.

--- tests/Browser/Http/Controllers/BlogController/SmokeTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-it('smoke tests the blog page', function () {
-    visit('/blog')
-        ->assertSee('Practical ideas for growing')
-        ->assertPresent('@blog-heading')
-        ->assertPresent('@blog-article-1');
-});
