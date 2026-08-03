a291e97b2756af882f4ac995f00c69d4c4d98953
rogerio-pereira <13219168+rogerio-pereira@users.noreply.github.com>
2026-08-03 13:15:11 +0000

    style(api/ui): Fix code style (pint and lint)

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)
     ->name('home');
 
-
 Route::post('/contact', [ContactController::class, 'store'])
     ->middleware('throttle:3,1') // max 3 contact submissions per IP per 1 minute
     ->name('contact.store');
